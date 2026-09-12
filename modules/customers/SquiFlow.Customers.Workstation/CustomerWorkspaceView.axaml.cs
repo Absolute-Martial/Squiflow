498beff5bbd02c2ef477101f5933f2ad8f3a6085
Daret dsat <bath-cackle-ember@duck.com>
2026-09-12 12:22:44 +0000

    Add host-specific module adapters and Core API skeleton

---
+++ modules/customers/SquiFlow.Customers.Workstation/CustomerWorkspaceView.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace SquiFlow.Customers.Workstation;
+
+public partial class CustomerWorkspaceView : UserControl
+{
+    public CustomerWorkspaceView() => InitializeComponent();
+}
