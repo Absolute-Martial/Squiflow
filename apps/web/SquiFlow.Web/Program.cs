44f4cac3cdcff185104b8507270d07c2c1a6683d
Daret dsat <bath-cackle-ember@duck.com>
2026-09-14 08:38:43 +0000

    phase0: rebuild thin tenant Web and CoreApi hosts

---
+++ apps/web/SquiFlow.Web/Program.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using SquiFlow.Observability.Logging;
+using SquiFlow.Web.Components;
+
+var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "0.0.0";
+using var logger = StructuredLogging.Create("SquiFlow.Web", version);
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+app.MapRazorComponents<App>();
+
+app.Lifetime.ApplicationStarted.Register(() => logger.Information("Tenant Web started"));
+app.Lifetime.ApplicationStopping.Register(() => logger.Information("Tenant Web stopping"));
+
+app.Run();
