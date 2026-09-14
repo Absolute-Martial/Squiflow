302b4d88154f4aa06a069805e4bcdd83fcfe8567
Daret dsat <bath-cackle-ember@duck.com>
2026-09-14 08:45:33 +0000

    phase0: add Web host liveness endpoint

--- apps/web/SquiFlow.Web/Program.cs
+++ apps/web/SquiFlow.Web/Program.cs
@@ -7,9 +7,11 @@ using var logger = StructuredLogging.Create("SquiFlow.Web", version);
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 app.MapRazorComponents<App>();
+app.MapHealthChecks("/health/live");
 
 app.Lifetime.ApplicationStarted.Register(() => logger.Information("Tenant Web started"));
 app.Lifetime.ApplicationStopping.Register(() => logger.Information("Tenant Web stopping"));
